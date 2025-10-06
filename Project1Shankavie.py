27ebab39d70ddf29ce3bb0abc5fe13d2a6587c99
Shankavie Gnaneswaran <sgnaneswaran@torontomu.ca>
2025-10-05 20:32:18 -0400

    Update Project1Shankavie.py

--- Project1Shankavie.py
+++ Project1Shankavie.py
@@ -1,7 +1,7 @@
 #AER850 - Project 1
 #Intro to Machine Learning  
 #Owner: Shankavie Gnaneswaran
-#Submission Date: Oct 10, 2025
+#Submission Date: Oct 11, 2025
 
 #Set up the imports 
 import pandas as pd
@@ -12,12 +12,13 @@ import numpy as np
 df = pd.read_csv('Project 1 data.csv') 
 
 #Preview the given data 
-print("Data Types and Coloumn Names:\n")
-print(df.info()) #data types and coloumn names 
+print("Data Types and Columns Names:\n")
+print(df.info()) #data types and columns names 
 print("First 5 Rows of the Data:\n")
 print(df.head()) #first 5 rows of the data
-print("Number of Rows and Coloumns:\n")
-print(df.shape) #number of rows and coloumns 
-print("Statistics for the Coloumns:\n")
-print(df.describe()) #statistics for the coloumns
+print("Number of Rows and Columns:\n")
+print(df.shape) #number of rows and columns 
+print("Statistics for the Columns:\n")
+print(df.describe()) #statistics for the columns
 
+#Plot and Explain the Data 
